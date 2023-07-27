dd5b49282a6a00c99a085a908f0a77292370d255
micaaaaahchu <136957109+micaaaaahchu@users.noreply.github.com>
2023-07-27 21:38:58 +0800

    Initial commit

---
+++ CustomerAAAPortlet/src/main/java/CustomerAAAPortlet/constants/CustomerAAAPortletKeys.java
@@ -0,0 +1,11 @@
+package CustomerAAAPortlet.constants;
+
+/**
+ * @author acera
+ */
+public class CustomerAAAPortletKeys {
+
+	public static final String CUSTOMERAAA =
+		"CustomerAAAPortlet_CustomerAAAPortlet";
+
+}
